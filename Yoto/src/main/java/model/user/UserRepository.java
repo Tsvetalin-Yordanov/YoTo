ef5bb7f8d4950d8222421fea91aedf140d6c792c
Kaloyan Todorov <kaltodor11@gmail.com>
2022-02-14 18:20:55 +0200

    merged commit

--- Yoto/src/main/java/model/user/UserRepository.java
+++ Yoto/src/main/java/model/user/UserRepository.java
@@ -8,11 +8,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
-
-<<<<<<< HEAD:Yoto/src/main/java/model/UserRepository.java
-=======
+    
     User findByEmail(String email);
    // User findByPhoneNumber(String phoneNumber);
     User findByEmailAndPassword(String email, String password);
->>>>>>> 691d112ce355261c35504be0625705714c5713b2:Yoto/src/main/java/model/user/UserRepository.java
 }
